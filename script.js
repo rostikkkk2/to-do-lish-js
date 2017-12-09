146df7a095099a11ae3fd9dd3d71bf25fe8d09ae
Rostik Yuriev <rost4402255@gmail.com>
2017-12-09 14:36:26 +0200

    redone js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementsByTagName('input')[0].addEventListener('keyup',
-  function(x){
+  function(e){
     var input = document.getElementsByTagName("input")[0].value;
-    if(x.keyCode == 13 && input != ""){
+    var ul = document.getElementsByTagName('ul')[0];
+    if(e.keyCode == 13 && input != ""){
       var li = document.createElement('li');
       li.innerText = input;
-      document.getElementsByTagName('ul')[0]
-      .insertBefore(li, document.getElementsByTagName('li')[0]);
-      document.getElementsByTagName("input")[0].value = "";//input.value = "" НЕ работает
-         }
+      ul.insertBefore(li, document.getElementsByTagName('li')[0]);
+      input.setAttribute('value',"");//не работает
+  }
 });
