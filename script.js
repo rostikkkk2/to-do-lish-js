42805ee793054e66521493db31f62c589210ad74
Rostik Yuriev <rost4402255@gmail.com>
2017-12-15 19:56:34 +0200

    redone js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementsByTagName('input')[0].addEventListener('keyup',
   function(e){
-    var input = document.getElementsByTagName("input")[0].value;
+    var input = document.getElementsByTagName("input")[0];
     var ul = document.getElementsByTagName('ul')[0];
-    if(e.keyCode == 13 && input != ""){
+    if(e.keyCode == 13 && input.value != ""){
       var li = document.createElement('li');
-      li.innerText = input;
+      li.innerText = input.value;
       ul.insertBefore(li, document.getElementsByTagName('li')[0]);
-      input.setAttribute('value',"");//не работает
+      input.value = "";
   }
 });
