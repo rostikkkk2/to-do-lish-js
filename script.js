0e3f461e3efb7065cfb525ab07d8a2314b8d8395
Rostik Yuriev <rost4402255@gmail.com>
2017-12-16 22:15:12 +0200

    add this

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.getElementsByTagName('input')[0].addEventListener('keyup',
   function(e){
-    var input = document.getElementsByTagName("input")[0];
-    var ul = document.getElementsByTagName('ul')[0];
-    if(e.keyCode == 13 && input.value != ""){
-      var li = document.createElement('li');
-      li.innerText = input.value;
-      ul.insertBefore(li, document.getElementsByTagName('li')[0]);
-      input.value = "";
+    this.ul = document.getElementsByTagName('ul')[0];
+    if(e.keyCode == 13 && this.value != ""){
+      this.li = document.createElement('li');
+      this.li.innerText = this.value;
+      this.ul.insertBefore(this.li, document.getElementsByTagName('li')[0]);
+      this.value = "";
   }
 });
