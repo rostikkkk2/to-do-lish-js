8ddec18c45c57aea19054115f3303af7293e19a2
Rostik Yuriev <rost4402255@gmail.com>
2017-12-18 22:56:58 +0200

    remove this

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementsByTagName('input')[0].addEventListener('keyup',
   function(e){
-    this.ul = document.getElementsByTagName('ul')[0];
+    var ul = document.getElementsByTagName('ul')[0];
     if(e.keyCode == 13 && this.value != ""){
-      this.li = document.createElement('li');
-      this.li.innerText = this.value;
-      this.ul.insertBefore(this.li, document.getElementsByTagName('li')[0]);
+      var li = document.createElement('li');
+      li.innerText = this.value;
+      ul.insertBefore(li, document.getElementsByTagName('li')[0]);
       this.value = "";
   }
 });
